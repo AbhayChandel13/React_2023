cea816ba34efd83ca45b42609a67b4cf7e9d827f
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-01-28 13:18:28 +0530

    basics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const App = () => {
+    const title = "I am title "
+    const paragraph = "hey, there i am the paragraph "
   return (
     <div>
       <h1> Hello , I am back at it </h1>
+      <h2 style={{'marginLeft':"50px"}}>{title}</h2>
+      <h3> {paragraph}</h3>
     </div>
   )
 }
