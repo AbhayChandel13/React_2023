2dc53cbc0248d0d2101ea2f62f06e43b69ab9b2f
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-06-28 15:37:44 +0530

    text  edited

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
     const paragraph = "hey, there i am the paragraph "
   return (
     <div>
-      <h1> Hello , I am back at it </h1>
+      <h1> Hello , I am back at it, Abhay Chandel </h1>
       <h2 style={{'marginLeft':"50px"}}>{title}</h2>
       <h3> {paragraph}</h3>
     </div>
