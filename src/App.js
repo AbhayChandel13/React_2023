2fc477200bc1a64ec8c50ecf751c21b74479b58d
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-06-28 15:52:16 +0530

    text edited

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 const App = () => {
     const title = "I am title "
-    const paragraph = "hey, there i am the paragraph "
+    const paragraph = "hey, there i am the paragraph no. one"
   return (
     <div>
-      <h1> Hello , I am back at it, Abhay Chandel </h1>
+      <h1> Hello ,    I am back at it, Abhay Chandel </h1>
       <h2 style={{'marginLeft':"50px"}}>{title}</h2>
       <h3> {paragraph}</h3>
-    </div>
+    </div>   
+
+
+    
   )
 }
 
